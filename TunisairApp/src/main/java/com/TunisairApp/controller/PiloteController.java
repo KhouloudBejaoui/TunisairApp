677cf0d223b7e902896fbac18314f29f2d7b0cd8
KhouloudBejaoui <bejaouikhouloud98@gmail.com>
2023-03-04 09:01:58 +0100

    connection to database and creation of the first table

---
+++ TunisairApp/src/main/java/com/TunisairApp/controller/PiloteController.java
@@ -0,0 +1,13 @@
+package com.TunisairApp.controller;
+
+import com.TunisairApp.service.PiloteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class PiloteController {
+    @Autowired
+    PiloteService piloteService;
+}
